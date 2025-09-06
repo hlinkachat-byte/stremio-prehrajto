04707e6b2e1457b4affa671db64215c97efe32bd
hlinkachat-byte <hlinka.chat@gmail.com>
2025-09-06 21:44:44 +0200

    Update stream.js

--- netlify/functions/stream.js
+++ netlify/functions/stream.js
@@ -1,9 +1,24 @@
 // netlify/functions/stream.js
-
 exports.handler = async (event) => {
-  const id = (event.queryStringParameters && event.queryStringParameters.id) || "";
+  // podpora oboch štýlov:
+  // 1) /stream/movie/tt1234567.json
+  // 2) /.netlify/functions/stream?id=tt1234567&type=movie
+  let id = "";
+  let type = "movie";
 
-  // Vraciame demo stream pre hocijaký film/seriál
+  // pokus o vyčítanie z path /stream/:type/:id.json (po redirecte z netlify.toml)
+  const m = event.path && event.path.match(/\/stream\/([^/]+)\/([^/.]+)\.json$/);
+  if (m) {
+    type = m[1];
+    id = m[2];
+  } else {
+    // fallback: query parametre
+    const qp = event.queryStringParameters || {};
+    id = qp.id || "";
+    type = qp.type || "movie";
+  }
+
+  // DEMO stream, aby sa addon zobrazil v zozname zdrojov pri každom titule
   const streams = [
     {
       title: "Prehraj.to demo stream",
