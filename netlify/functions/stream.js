171b16637330507cb006d4f8b70b41b28ec16183
hlinkachat-byte <hlinka.chat@gmail.com>
2025-09-06 22:58:58 +0200

    Update stream.js

--- netlify/functions/stream.js
+++ netlify/functions/stream.js
@@ -1,24 +1,18 @@
 // netlify/functions/stream.js
 exports.handler = async (event) => {
-  // podpora oboch štýlov:
-  // 1) /stream/movie/tt1234567.json
-  // 2) /.netlify/functions/stream?id=tt1234567&type=movie
+  // podpora /stream/movie/tt1234567.json aj query ?id=&type=
   let id = "";
   let type = "movie";
-
-  // pokus o vyčítanie z path /stream/:type/:id.json (po redirecte z netlify.toml)
   const m = event.path && event.path.match(/\/stream\/([^/]+)\/([^/.]+)\.json$/);
   if (m) {
     type = m[1];
     id = m[2];
   } else {
-    // fallback: query parametre
     const qp = event.queryStringParameters || {};
     id = qp.id || "";
     type = qp.type || "movie";
   }
 
-  // DEMO stream, aby sa addon zobrazil v zozname zdrojov pri každom titule
   const streams = [
     {
       title: "Prehraj.to demo stream",
@@ -33,6 +27,6 @@ exports.handler = async (event) => {
       "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Headers": "Content-Type"
     },
-    body: JSON.stringify({ streams })
+    body: JSON.stringify({ streams })   // <-- DÔLEŽITÉ: objekt s kľúčom "streams"
   };
 };
